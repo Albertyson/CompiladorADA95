68271d78da0f63917e3d7653e1e149b4a39e0fd3
Josue Rojas <josue.rojas@unitec.edu>
2016-03-31 07:42:11 -0600

    resolviendo conflictos2

---
+++ CompiladorADA95/src/mainPackage/pruebas adb/Con Error/error1.adb
@@ -0,0 +1,34 @@
+procedure Hola is
+
+    x,a, : integer ;
+
+begin 
+    
+    x := 5;
+    get (a);
+    put (x);
+    
+    while a > 5 loop
+        Pointer := 3;
+        exit when a = b;
+    end ;
+
+    for i in 0..5 loop
+        s := i;
+    end loop;
+
+
+
+    case X is
+        when a    => Item := 1;
+        when b    => Item := 2;
+        when c    => Item := 3;
+        when d    => Item := 4;
+        when others => Item := 5;
+    end case;
+
+
+
+
+
+end Hola;
